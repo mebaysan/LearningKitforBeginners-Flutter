35e7646e2e08ea84766c3d4f1fac2c00f6dccde8
mebaysan <menesbaysan@gmail.com>
2019-09-10 15:28:49 +0300

    a

---
+++ 9-Ecommerce/ecommerce/lib/screens/productDetail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ProductDetailState();
+}
+
+class _ProductDetailState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
