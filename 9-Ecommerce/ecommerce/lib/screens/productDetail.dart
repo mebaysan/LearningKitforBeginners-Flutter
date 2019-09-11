35062ed445ef0b32b15ba1f427b2a0121e383b37
mebaysan <menesbaysan@gmail.com>
2019-09-11 12:02:06 +0300

    e-commerce design

--- 9-Ecommerce/ecommerce/lib/screens/productDetail.dart
+++ 9-Ecommerce/ecommerce/lib/screens/productDetail.dart
@@ -5,9 +5,312 @@ class ProductDetail extends StatefulWidget {
   State<StatefulWidget> createState() => _ProductDetailState();
 }
 
-class _ProductDetailState extends State {
+class _ProductDetailState extends State with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Product Detail",
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            size: 40.0,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: _buildProductDetails(context),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  _buildProductDetails(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ListView(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(4.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _buildProductImages(),
+              _buildProductTitle(),
+              SizedBox(
+                height: 12.0,
+              ),
+              _buildProductPrice(),
+              SizedBox(
+                height: 12.0,
+              ),
+              _buildDivider(size),
+              SizedBox(
+                height: 12.0,
+              ),
+              _buildFurtherInfo(),
+              SizedBox(
+                height: 12.0,
+              ),
+              _buildDivider(size),
+              SizedBox(
+                height: 12.0,
+              ),
+              _buildSizeArea(),
+              SizedBox(
+                height: 12.0,
+              ),
+              _buildInfo(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  _buildProductImages() {
+    TabController imagesController = TabController(length: 3, vsync: this);
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Container(
+        height: 250.0,
+        child: Center(
+          child: DefaultTabController(
+            length: 3,
+            child: Stack(
+              children: <Widget>[
+                TabBarView(
+                  controller: imagesController,
+                  children: <Widget>[
+                    Image.network(
+                        "https://m.media-amazon.com/images/I/61Kezcz3VAL._SR500,500_.jpg"),
+                    Image.network(
+                        "https://m.media-amazon.com/images/I/61Kezcz3VAL._SR500,500_.jpg"),
+                    Image.network(
+                        "https://m.media-amazon.com/images/I/61Kezcz3VAL._SR500,500_.jpg"),
+                  ],
+                ),
+                Container(
+                  alignment: FractionalOffset(0.5, 0.95),
+                  child: TabPageSelector(
+                    controller: imagesController,
+                    selectedColor: Colors.grey,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildProductTitle() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      child: Center(
+        child: Text(
+          "Kazak",
+          style: TextStyle(fontSize: 16.0, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  _buildProductPrice() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        children: <Widget>[
+          Text(
+            "price",
+            style: TextStyle(fontSize: 16.0, color: Colors.black),
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Text(
+            "old price",
+            style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey,
+                decoration: TextDecoration.lineThrough),
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Text(
+            "discount",
+            style: TextStyle(fontSize: 12.0, color: Colors.blue),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildDivider(Size screenSize) {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.grey,
+          width: screenSize.width,
+          height: 0.25,
+        ),
+      ],
+    );
+  }
+
+  _buildFurtherInfo() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        children: <Widget>[
+          Icon(Icons.local_offer, color: Colors.grey),
+          SizedBox(
+            width: 12.0,
+          ),
+          Text(
+            "Click for More Info",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildSizeArea() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.straighten,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Text(
+                "Size : XL",
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                "Size Table",
+                style: TextStyle(fontSize: 12.0, color: Colors.blue),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildInfo() {
+    TabController tabController = TabController(length: 2, vsync: this);
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TabBar(
+            controller: tabController,
+            tabs: <Widget>[
+              Tab(
+                child: Text(
+                  "Product Detail",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Wash Detail",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+            height: 40.0,
+            child: TabBarView(
+              controller: tabController,
+              children: <Widget>[
+                Text(
+                  "%50 discount and fresh tshirt it is the best tshirt",
+                  style: TextStyle(color: Colors.black),
+                ),
+                Text(
+                  "Need to 30^ wash",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildBottomNavigationBar() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.blueAccent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.list, color: Colors.white),
+                      SizedBox(
+                        width: 4.0,
+                      ),
+                      Text(
+                        "Add Fav",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ))),
+          Flexible(
+              fit: FlexFit.tight,
+              flex: 1,
+              child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.greenAccent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.shopping_cart, color: Colors.white),
+                      SizedBox(
+                        width: 4.0,
+                      ),
+                      Text(
+                        "Add Cart",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ))),
+        ],
+      ),
+    );
   }
 }
